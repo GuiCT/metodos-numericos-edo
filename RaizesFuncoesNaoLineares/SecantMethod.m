9aabd8af7e51eb5a4f2cdb3504513082c7423bd5
Guilherme Tomiasi <gc.tomiasi@unesp.br>
2022-07-11 21:14:31 -0300

    finalizando Método da Secante

--- RaizesFuncoesNaoLineares/SecantMethod.m
+++ RaizesFuncoesNaoLineares/SecantMethod.m
@@ -1,5 +1,36 @@
-function z = SecantMethod(f, z1, tol)
-% Executa método da Secante
-isWithinTolerance = @(value) abs(value) < tol;
-
+function [z_h, f_h] = SecantMethod(f, z0, z1, tol, maxitr)
+% Executa método da Secante para uma determinada função f.
+% Guarda valores de z e f(z) para cada z.
+% Útil para mostrar a convergência de cada passo.
+f0 = f(z0);
+f1 = f(z1);
+z_h = [z0;z1];
+f_h = [f0;f1];
+itr = 0;
+while itr < maxitr
+    % Encontra novo z a partir do Método da Secante
+    z2 = secant_method_step(f0, f1, z0, z1);
+    % Se z2 for NaN (ocorre quando f(z1) = f(z0)), encerra.
+    % Não garante a convergência.
+    if isnan(z2)
+        break
+    end
+    % Encontra novo f(z)
+    f2 = f(z2);
+    % Adiciona valores ao histórico.
+    z_h = [z_h;z2];
+    f_h = [f_h;f2];
+    % Se f2 está suficientemente perto de zero dada a tolerância
+    % especificada, encerra.
+    if abs(f2) < tol
+        break
+    end
+    % Atualizando z0, z1, f0 e f1.
+    z0 = z1;
+    f0 = f1;
+    z1 = z2;
+    f1 = f2;
+    % Próxima iteração.
+    itr = itr + 1;
+end
 end
--- RaizesFuncoesNaoLineares/private/secant_method_step.m
+++ RaizesFuncoesNaoLineares/private/secant_method_step.m
@@ -1,10 +1,4 @@
-function z = secant_method_step(f, z0, z1)
-if z1 < z0
-    aux = z1;
-    z1 = z0;
-    z0 = aux;
+function z = secant_method_step(f0, f1, z0, z1)
+numerical_derivative = (f1-f0)/(z1-z0);
+z = z1 - f1/numerical_derivative;
 end
-numerical_derivative = (z1-z0)/(f(z1)-f(z0));
-z = z1 - f(z1)*numerical_derivative;
-end
-
